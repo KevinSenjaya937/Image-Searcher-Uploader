97005094e03947fe4420ca7fc961c65d06e5a3d7
Kevin Senjaya <kevin.senjaya937@gmail.com>
2022-10-14 15:01:50 +0800

    Images Saved on Device DONE

--- app/src/main/java/au/edu/curtin/reactiveandroid/MainActivity.kt
+++ app/src/main/java/au/edu/curtin/reactiveandroid/MainActivity.kt
@@ -1,7 +1,13 @@
 package au.edu.curtin.reactiveandroid
 
+import android.content.ContentValues
+import android.content.Context
 import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -22,6 +28,10 @@ import io.reactivex.rxjava3.core.SingleObserver
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         uploadBtn.setOnClickListener {
-            uploadImages()
+            saveImages()
         }
     }
 
@@ -164,6 +174,66 @@ class MainActivity : AppCompatActivity() {
 
     fun saveImages() {
 
+        for (image in imageController.getToUploadList()) {
+            saveImage(image, this, "ReactiveAndroid")
+        }
+    }
 
+
+
+    private fun saveImage(bitmap: Bitmap, context: Context, folderName: String) {
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            Log.d("KEVINTEST", "We Here!")
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + folderName)
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            // RELATIVE_PATH and IS_PENDING are introduced in API 29.
+
+            val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            Log.d("KEVINTEST", uri.toString())
+            if (uri != null) {
+                saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                context.contentResolver.update(uri, values, null, null)
+            }
+        } else {
+            val directory = File(Environment.getExternalStorageDirectory().toString() + separator + folderName)
+            // getExternalStorageDirectory is deprecated in API 29
+
+            if (!directory.exists()) {
+                directory.mkdirs()
+            }
+            val fileName = System.currentTimeMillis().toString() + ".png"
+
+            Log.d("KEVINTEST", "Filename:$fileName")
+            val file = File(directory, fileName)
+            saveImageToStream(bitmap, FileOutputStream(file))
+            if (file.absolutePath != null) {
+                val values = contentValues()
+                values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+                Log.d("KEVINTEST", "File Absolute Path:${ file.absolutePath }")
+                // .DATA is deprecated in API 29
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            }
+        }
+    }
+
+    private fun contentValues() : ContentValues {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        return values
+    }
+
+    private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
